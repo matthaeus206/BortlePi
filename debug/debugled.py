d234cb9a4d24944f5d33d85631115aa65eaf9c27
Matt <60934578+matthaeus206@users.noreply.github.com>
2025-05-15 11:37:07 -0700

    Create debugled.py

---
+++ debug/debugled.py
@@ -0,0 +1,8 @@
+import board, digitalio, time
+
+led = digitalio.DigitalInOut(board.GP25)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = not led.value
+    time.sleep(0.5)
